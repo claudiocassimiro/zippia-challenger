d967650ae45f6d61951dd9102f93383968902d03
claudiocassimiro <days_surfing@live.com>
2023-02-16 11:29:39 -0300

    feat: implements some interfaces

---
+++ src/utils/types.ts
@@ -0,0 +1,17 @@
+export interface WindowSizes {
+  width: number;
+  height: number;
+  isMobile: boolean;
+  isHorizontal: boolean;
+  isDesktop: boolean;
+  isTablet: boolean;
+}
+
+export interface Jobs {
+  jobId: string;
+  jobTitle: string;
+  companyName: string;
+  jobDescription: string;
+  postedDate: string;
+  postingDate: string;
+}
